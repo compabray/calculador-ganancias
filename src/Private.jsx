3538d9f30a891f74c5aa52a0085fcd0a72f04faf
Brayan Rodriguez <brayanignacio.06.10@gmail.com>
2023-02-06 17:31:27 -0300

    feat: sending data to server

--- src/Private.jsx
+++ src/Private.jsx
@@ -1,20 +1,25 @@
 
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import { collection, getDocs, query, where, onSnapshot } from "firebase/firestore";
+import firebase from 'firebase/compat/app';
+import { collection, query, where, onSnapshot, setDoc, doc } from "firebase/firestore";
 import {useState, useEffect} from "react";
 import { auth, db} from "./firebase-config";
 
 
 function Private ({user}){
 
+    //State to store the data from the database
     const [ingresos, setIngresos] = useState([]);
     const [gastos, setGastos] = useState([]);
     const historial = ingresos.concat(gastos);
 
+    //Sort the data by date
     const historialSort = historial.sort((a, b) => {
         return b.date - a.date;
     });
+
+    //Get the data from the database
 
     useEffect(() => {
         //Get the data from the database
@@ -37,19 +42,23 @@ function Private ({user}){
             unsubscribeIngresos();
             unsubscribeGastos();
         };
-
-
 // eslint-disable-next-line react-hooks/exhaustive-deps
 }, []);
 
+
+
+    //Toggle the state to show or hide the data
+    const [toggleGastos, setToggleGastos] = useState(false);
+    const [toggleIngresos, setToggleIngresos] = useState(false);
+
     return (
         <div>
-            <div className='flex justify-between'>
+            <div className='flex justify-between w-full'>
                 <h1 className='text-xl text-indigo-400 p-4'> Comienza a Calcular tus ganancias ya!</h1>
                 {auth.currentUser && <button className='px-4 py-2 m-4 bg-zinc-900 font-semibold rounded text-indigo-500' onClick={() => auth.signOut()}>Cerrar sesion</button>}
             </div>
            
-   
+        <div>
         <div className='w-1/6'>
             <h2 className='text-lg text-zinc-500 text-center'>Historial:</h2>
             {historialSort.map((data) => {
@@ -76,7 +85,30 @@ function Private ({user}){
             
         )
     })}</div>
+
+        <div className='w-1/5'>
+            <h2 className='text-lg text-zinc-500 text-center'>Agrega nueva actividad de tu negocio!</h2>
+            <div> 
+                <button className='text-red-200' onClick={() => setToggleGastos(!toggleGastos)}>Nuevo Gasto!</button>
+                {toggleGastos && (
+                    <div className='flex flex-col'>
+                        <input type="text" placeholder='Fuente del gasto' id='fuenteGasto' />
+                        <input type="number" placeholder='Valor del gasto' id='valorGasto' />
+                        <button className="text-red-200" onClick={() => {
+                            setDoc(doc(db, "gastos", user.uid), {
+                                fuente: document.getElementById('fuenteGasto').value,
+                                valor: document.getElementById('valorGasto').value,
+                                gasto: true,
+                                uid: user.uid,
+                                date: new Date(),
+                            })
+                        }}>Agregar Gasto</button>
+                    </div>
+                )}	
+            </div>
         </div>
+        </div>
+    </div>
     )
     //⥄
 }
