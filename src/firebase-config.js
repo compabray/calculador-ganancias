5b711fbfeff0915cd1c76606f66a16a366fd220d
Brayan Rodriguez <brayanignacio.06.10@gmail.com>
2023-02-11 00:37:54 -0300

    feat: firebase keys en archivo fuera de github

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,17 +1,9 @@
-
+import {apiKey} from './apiKey.js';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-const firebaseApp = firebase.initializeApp( {
-  apiKey: "AIzaSyCi8O1ELlnHEMbKPo6W8-PJDV60nQdqyJM",
-  authDomain: "calculadora-de-ganancias.firebaseapp.com",
-  projectId: "calculadora-de-ganancias",
-  storageBucket: "calculadora-de-ganancias.appspot.com",
-  messagingSenderId: "666324250754",
-  appId: "1:666324250754:web:460e65331e173e4a8c449b",
-  measurementId: "G-86922JM6KG"
-})
+const firebaseApp = firebase.initializeApp(apiKey)
 
 export const auth = firebaseApp.auth()
 export const db = firebaseApp.firestore()
