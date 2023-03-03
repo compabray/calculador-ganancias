e682d555c2c27d76640bbbdba22bb309acba8fc3
Brayan Rodriguez <brayanignacio.06.10@gmail.com>
2023-03-03 01:16:17 -0300

    feat: grupo cards design

--- src/pages/Group.jsx
+++ src/pages/Group.jsx
@@ -1,7 +1,8 @@
 import {useState, useEffect} from 'react';
 import { useLocation, Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 
 function Group() {
@@ -26,15 +27,21 @@ function Group() {
                     const fireBaseTime = new Date(item.date.seconds * 1000 + item.date.nanoseconds / 1000000,);
                     const date = fireBaseTime.toLocaleDateString();
                 return (
-                    <div key={item.fuente} className=" w-1/4 bg-zinc-900 text-center rounded-lg h-auto items-stretch">   
+                    <div key={item.fuente} className="flex flex-col w-1/4 bg-zinc-900 text-center rounded-lg h-auto items-stretch">   
                         
                         <h2 className='text-xl text-zinc-300 font-bold w-full rounded-t-lg bg-zinc-800 h-16 truncate py-5 px-2'>{item.fuente.charAt(0).toUpperCase() + item.fuente.slice(1)}</h2>
                       
            
-                        <div className='p-4 w-full'>
-                            <h3 className={`${item.gasto === true ? 'text-red-500' : 'text-green-500'}  w-full  text-xl`}>{item.gasto === true ? ` - $${item.valor}` : ` + $${item.valor}`}</h3>
-                            <h2 className='w-full text-center text-zinc-500 text-sm'>{date}</h2>
-                            <h3 className='text-zinc-400 mt-4 multine-ellipsis' >{ item.descripcion ? `${item.descripcion.charAt(0).toUpperCase() + item.descripcion.slice(1).toLowerCase()}` : 'Aún no tiene una descripción' } </h3>
+                        <div className='flex flex-col justify-between  p-4 w-full flex-1'>
+                            <div>
+                                <h3 className={`${item.gasto === true ? 'text-red-500' : 'text-green-500'}  w-full  text-xl`}>{item.gasto === true ? ` - $${item.valor}` : ` + $${item.valor}`}</h3>
+                                <h2 className='w-full text-center text-zinc-500 text-sm'>{date}</h2>
+                                <h3 className='text-zinc-400 mt-4 multine-ellipsis' >{ item.descripcion ? `${item.descripcion.charAt(0).toUpperCase() + item.descripcion.slice(1).toLowerCase()}` : 'Aún no tiene una descripción' } </h3>
+                            </div>
+                            <div className='w-full flex justify-around mt-3'>
+                                <Link className='w-2/5 text-indigo-500'><FontAwesomeIcon icon={faPencil}/> Editar</Link>
+                                <button className='w-2/5 text-indigo-500'><FontAwesomeIcon icon={faTrash}/> Borrar</button>
+                            </div>
                         </div>
                     </div>
                 )})
